9d992ae5d901e3fe1b37e49554fcfe7d98072e64
AlishaMahek03 <mahekalisha03@gmail.com>
2025-05-21 12:29:00 -0300

    added URL in app.js

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://your-frontend.onrender.com'
+  'https://uber-clone-4u0q.onrender.com'
 ];
 
 app.use(cors({
