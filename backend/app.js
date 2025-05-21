f54cd16cc1e74a6bbea7d4e5b1699a8942acce3e
AlishaMahek03 <mahekalisha03@gmail.com>
2025-05-21 09:37:57 -0300

    kds

--- backend/app.js
+++ backend/app.js
@@ -8,29 +8,14 @@ const connectDB = require("./db/db");
 connectDB();
 const mapsRoutes = require("./routes/maps.routes");
 const rideRoutes = require("./routes/ride.route");
-
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://uber-clone-91h6.onrender.com', // <-- your deployed frontend
-  // add any other frontend URLs you use
-];
-
-app.use(cors({
-  origin: function(origin, callback) {
-    console.log('CORS request from:', origin);
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 const userRoutes = require("./routes/user.routes");
 const captainRoutes = require("./routes/captain.routes");
 
